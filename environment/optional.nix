be10f0fa817bea2fbdd98c1b27be54e0bb904709
Togo-GT <michael.kaare.nielsen@gmail.com>
2025-11-20 21:30:52 +0100

    a

--- environment/optional.nix
+++ environment/optional.nix
@@ -1,15 +1,15 @@
-# environment/systemPackages/optional.nix
+# environment/optional.nix
 
 { pkgs, lib }:
 
 let
-  core = import ./core.nix { inherit pkgs lib; };
-  cli = import ./cli.nix { inherit pkgs lib; };
+  core = import ./systemPackages/core.nix { inherit pkgs lib; };
+  cli = import ./systemPackages/cli.nix { inherit pkgs lib; };
 #  dev = import ./dev.nix { inherit pkgs lib; };
 #  gui = import ./gui.nix { inherit pkgs lib; };
 #  gaming = import ./gaming.nix { inherit pkgs lib; };
 #  multimedia = import ./multimedia.nix { inherit pkgs lib; };
-  system = import ./system.nix { inherit pkgs lib; };
+  system = import ./systemPackages/system.nix { inherit pkgs lib; };
   essential = import ./systemPackages/essential.nix { inherit pkgs lib; };
 
 in
