30bb4e797c6102c3e13e41cd8ad458ad50a72214
Togo-GT <michael.kaare.nielsen@gmail.com>
2025-11-24 18:32:46 +0100

    f

--- environment/iso-configuration.nix
+++ environment/iso-configuration.nix
@@ -125,7 +125,7 @@
   fonts.packages = with pkgs; [
     noto-fonts
     noto-fonts-cjk-sans
-    noto-fonts-emoji
+    noto-fonts-color-emoji # update
     fira-code  # Use regular fira-code instead of nerd-fonts
   ];
 
